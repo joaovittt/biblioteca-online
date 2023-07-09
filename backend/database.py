fb3b4602cb6a20c66c94844818d91a09402b60af
joaovittt <jvleonidio2001@gmail.comc:/Users/Joo Vitor/Documents/biblioteca-online/venv/Scripts/python.exe>
2023-07-09 18:59:38 -0300

    cria a engine para conexão

--- backend/database.py
+++ backend/database.py
@@ -2,4 +2,15 @@ import sqlalchemy as sql
 import sqlalchemy.ext.declarative as declarative 
 import sqlalchemy.orm as orm 
 
-DATABASE_URL = 
+
+db_endpoint = 'database-1.clllywvqw34m.us-east-2.rds.amazonaws.com'
+db_port = 5432
+db_name = 'postgres'
+db_user = 'postgres'
+db_password = 'biblio123'
+
+
+DATABASE_URL = f'postgresql://{db_user}:{db_password}@{db_endpoint}:{db_port}/{db_name}'
+
+
+engine = sql.create_engine(DATABASE_URL)
